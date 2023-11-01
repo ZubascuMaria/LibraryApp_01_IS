403928d134adea52e86cad174bbc569547044f62
ZubascuMaria <115020459+ZubascuMaria@users.noreply.github.com>
2023-11-01 19:23:10 +0200

    Update JDBConnectionWrapper.java

--- src/main/java/database/JDBConnectionWrapper.java
+++ src/main/java/database/JDBConnectionWrapper.java
@@ -11,7 +11,7 @@ public class JDBConnectionWrapper {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/";
 
     private static final String USER = "root";
-    private static final String PASSWORD = "Maria300*";
+    private static final String PASSWORD = "";
     private static final int TIMEOUT = 5;
 
 
